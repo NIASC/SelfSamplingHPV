a514f961de3ccb3bd0728c6e6c59366877bb17cb
Zurab <zurab.bzhalava@ki.se>
2017-01-13 18:45:45 +0100

    my commit

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,9 +1,9 @@
 <?php
 
          $host = "localhost";          //address to the server where database resides
-         $user = "user";            //login id
-         $pw   = "password";                //password
-         $database = "database_name";     //database name
+         $user = "root";            //login id
+         $pw   = "root";                //password
+         $database = "hpvcente_test_order_database";     //database name
 
          $db = mysql_connect($host,$user, $pw) or die("Cannot connect to MySQL.");
   
